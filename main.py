fc9cb97837f7597afc8bcda1ec650a5930fbde36
ROKKAM SAIKUMAR <96229847+saikuma1r@users.noreply.github.com>
2025-04-11 00:36:58 +0530

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-# main.py
-from kivy.app import App
-from kivy.uix.label import Label
-
-class MyApp(App):
-    def build(self):
-        return Label(text="Hello from Kivy on Android!")
-
-MyApp().run()
